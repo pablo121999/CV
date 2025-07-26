aa8a878167a54e3fd3c42578d47c6f27e2ac9127
paroca <paroca@nexura.com>
2025-07-26 15:39:34 -0500

    timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,19 +1,28 @@
 var timelineSwiper = new Swiper('.timeline .swiper-container', {
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: false,
     speed: 1600,
     pagination: '.swiper-pagination',
-    paginationBulletRender: function(swiper, index, className) {
+    paginationClickable: true,
+    paginationBulletRender: function (swiper, index, className) {
         var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
         return '<span class="' + className + '">' + year + '&nbsp;&nbsp;</span>';
     },
-    paginationClickable: true,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
-    mousewheel: true,
+    mousewheelControl: true, // <-- Esta es la clave para Swiper 3
     breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
         768: {
-            direction: 'horizontal',
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 30
         }
     }
 });
